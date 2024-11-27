7e8701e37f1edf7dd7d490fd435c8d76944cb882
Kusunoki9999 <To29Sku51@icloud.com>
2024-11-27 09:11:52 +0900

    exifデータ取得テスト

---
+++ test/exif_test.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from PIL.ExifTags import TAGS,GPSTAGS
+import pandas as pd
+
+path = "test_image.JPG"
+
+def load_exif(path):
+    ifd_dict = {}
+    with Image.open(path) as im:
+        exif = im.getexif()
+    ifd_dict["Exif"] = exif.get_ifd(0x8769) #0x8769がExifデータへのアクセスタグ番号
+    ifd_dict["GPSInfo"] = exif.get_ifd(0x8825) #0x8825がGPSデータ
+    return ifd_dict
+
+ifd_dict = load_exif(path)
+exif_sr = pd.Series(ifd_dict["Exif"]).rename(TAGS)
+gps_sr = pd.Series(ifd_dict["GPSInfo"]).rename(GPSTAGS)
+
+print(gps_sr)
