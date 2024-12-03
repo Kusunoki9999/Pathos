ea74ab9423913b35213842f954130ed0d666c9a4
Kusunoki9999 <To29Sku51@icloud.com>
2024-12-03 12:01:14 +0900

    APIキーを.envファイルから取得するAPIをapp.pyに実装

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from dotenv import load_dotenv
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
+GOOGLE_MAP_API_KEY = os.getenv("GOOGLE_MAP_API_KEY")
+
+app.mount("/", StaticFiles(directory="path/to/your/html/directory", html=True), name="static")
 
 @app.get("/")
 async def root():
     return {"message":"Hello,world"}
+
+@app.get("/api-key")
+async def get_api_key():
+    return {"api_key":GOOGLE_MAP_API_KEY}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, log_level="debug")
