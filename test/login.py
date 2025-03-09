c80ce87010cc0281f436aaf28fc660006ef4a1b3
Kusunoki9999 <To29Sku51@icloud.com>
2025-03-09 12:07:18 +0900

    test用ユーザー情報の用意

--- test/login.py
+++ test/login.py
@@ -14,6 +14,15 @@ class User(BaseModel):
     full_name: Union[str, None] = None
     disabled: Union[bool, None] = None
 
-@app.get("/items/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="Kusunoki9999@example.com", full_name="Kusunoki Taro"
+    )
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
